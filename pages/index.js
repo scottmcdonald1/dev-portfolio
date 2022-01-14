4d2abc5fef991326de3df8270eef1dbfd8d5135c
scoopd0303 <scott.russell.mcdonald@gmail.com>
2022-01-14 17:06:50 -0600

    reworked/added SocialIcons component using svgs as components. slowly reworking layout.

--- pages/index.js
+++ pages/index.js
@@ -4,58 +4,38 @@ import { useState } from 'react'
 
 import Projects from '../components/Projects'
 import About from '../components/About'
-
-import expandMore from '../public/expand_more.svg'
-import expandLess from '../public/expand_less.svg'
-import githubIcon from '../public/github.svg'
-import linkedinIcon from '../public/linkedin.svg'
-import profilePic from '../public/profilepic.jpeg'
+import SocialIcons from '../components/SocialIcons'
 
 export default function Home() {
   return (
-    <div>
+    <>
       <Head>
         <title>Scott McDonald</title>
       </Head>
 
-      <div className='w-screen grid gap-2 justify-center items-center px-2 sm:px-8 pb-20 pt-32'>
-
-        <div className='bg-white fixed top-2 left-2 grid gap-4 grid-cols-2 justify-center items-center py-2 px-4 border border-ombreNaturelle31 rounded shadow-sharp'>
-
-          <a 
-            href='https://github.com/scottmcdonald1' 
-            target="_blank" 
-            rel='noreferrer'
-            className='grid items-center'
-          >
-            <Image src={githubIcon} width={40} height={40} alt='github icon' />
-          </a>
-
-          <a 
-            href='https://www.linkedin.com/in/scott-mcdonald-0303/' 
-            target="_blank" 
-            rel='noreferrer'
-            className='grid items-center'
-          >
-            <Image src={linkedinIcon} width={40} height={40} className='fill-gray-500' alt='linkedin icon' />
-          </a>
-
-        </div>
+      <div className='w-screen p-2'>
       
-        <div className='bg-white grid justify-center items-center border border-ombreNaturelle31 rounded px-4 pt-8 pb-4 shadow-sharp'>
+        <div className='w-full grid grid-cols-3'>
 
-          <div className='grid justify-center'>
-            <h1 className='font-bowlbyOneSC text-3xl sm:text-5xl lg:text-6xl'>
-              Scott McDonald
-            </h1>
+          <div className='flex'>
+            <SocialIcons />
           </div>
 
-          <div className='grid justify-end px-5'>
-            <h2 className='font-monda text-md md:text-lg text-ombreNaturelle31/70'>
-              Web Developer for hire
-            </h2>
+          <div className='h-full flex justify-center items-center'>
+            <div className='bg-white h-full grid justify-center items-center border border-ombreNaturelle31 rounded  shadow-sharp'>
+              <h1 className='font-bowlbyOneSC text-2xl whitespace-nowrap'>
+                Scott McDonald
+              </h1>
+            </div>
           </div>
-            
+
+          <div className='h-full grid justify-center items-center'>
+            <div className='bg-white h-full grid justify-center items-center border border-ombreNaturelle31 rounded shadow-sharp'>
+              <h1 className='font-monda text-xl'>
+                about
+              </h1>
+            </div>
+          </div>
 
         </div>
 
@@ -72,6 +52,6 @@ export default function Home() {
         </div>
       </div>
 
-    </div>
+    </>
   )
 }
