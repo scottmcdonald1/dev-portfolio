5c3446ffe71a99b3318ef78e7b65cef7567abb26
Scott McDonald <scottmcdonald@Scotts-MacBook-Air.local>
2022-01-24 11:14:23 -0600

    build fixes for deployment

--- pages/index.js
+++ pages/index.js
@@ -18,17 +18,7 @@ export default function Home() {
 
           <div className='bg-white/70 w-full grid gap-3 border border-ombreNaturelle31 rounded shadow-sharp px-6 py-4'>
 
-            <p>
-              👋 Hello! Welcome to my portfolio page. My name is Scott McDonald, and I have been working incredibly hard towards becoming a web developer, and I have created this page to showcase the skills that I have been developing over the course of the past two years.
-            </p>
-
-            <p>
-              This site was made in React using a Next.js framework, using Tailwind.CSS for styling, and is currently hosted on Vercel. I would like to note that this site is currently (and may forever be) in development, as I intend to use it as a sort of creative playground for new found skills and design concepts -- so you may see some changes should you revisit. 
-            </p>
-
-            <p>
-              Please check out the about page for a brief summary of my journey in coding thus far, and be sure to pull up the project page for examples of my work. I am an artist by hobby, so I've included an artwork page with a few examples of my work.
-            </p>
+            <WelcomeMessage message={welcome} />
 
           </div>
 
@@ -39,3 +29,24 @@ export default function Home() {
     </>
   )
 }
+
+function WelcomeMessage({message}) {
+
+  const paragraphs = message.map((paragraph, i) => {
+    return <p key={i}>{paragraph}</p>
+  })
+
+  return <>{paragraphs}</>
+}
+
+const welcome = [
+  `
+    👋 Hello! Welcome to my portfolio page. My name is Scott McDonald, and I have been working incredibly hard towards becoming a web developer, and I have created this page to showcase the skills that I have been developing over the course of the past two years.
+  `,
+  `
+    This site was made in React using a Next.js framework, using Tailwind.CSS for styling, and is currently hosted on Vercel. I would like to note that this site is currently (and may forever be) in development, as I intend to use it as a sort of creative playground for new found skills and design concepts -- so you may see some changes should you revisit. 
+  `,
+  `
+    Please check out the about page for a brief summary of my journey in coding thus far, and be sure to pull up the project page for examples of my work. I am an artist by hobby, so I've included an artwork page with a few examples of my work.
+  `,
+]
