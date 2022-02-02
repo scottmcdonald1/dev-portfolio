ee04aa4bc9834b61fb9d9a68601de28284482cf6
Scott McDonald <scottmcdonald@Scotts-MacBook-Air.local>
2022-02-02 14:30:26 -0600

    fixed build errors

--- pages/index.js
+++ pages/index.js
@@ -39,7 +39,7 @@ export default function Home() {
                   <div className='bg-white grid gap-2 border border-ombreNaturelle31 rounded shadow-sharp px-4 py-4'>
                     <h2 className='text-xl'>My name is Scott McDonald <span className='text-sm'>(he/him)</span></h2>
                     <p className='max-w-prose text-justify text-ombreNaturelle31 px-3'>
-                       I'm a full stack developer and artist. I have created this site to showcase the skills that I have been <i>developing</i> over the past two years.
+                       I&apos;m a full stack developer and artist. I have created this site to showcase the skills that I have been <i>developing</i> over the past two years.
                     </p>
                   </div>
                 </div>
@@ -54,11 +54,11 @@ export default function Home() {
                 <div>
                   <div className='bg-white grid gap-2 border border-ombreNaturelle31 rounded shadow-sharp px-6 py-4'>
                     <h2 className='text-lg'>
-                      Come take a look!
+                      Feel free to look around!
                     </h2>
 
                     <p className='max-w-prose text-justify text-ombreNaturelle31 px-3'>
-                      If you'd like you can learn a little bit more <Link href="/about"><span className='text-orangeVif underline hover:cursor-pointer'>about me</span></Link>, take a peek at some of my <Link href="/projects"><span className='text-orangeVif underline hover:cursor-pointer'>portfolio projects</span></Link>, or check out some of my <Link href="/artwork"><span className='text-orangeVif underline hover:cursor-pointer'>strange artwork</span></Link>.
+                      If you&apos;d like you can learn a little bit more <Link href="/about"><span className='text-orangeVif underline hover:cursor-pointer'>about me</span></Link>, take a peek at some of my <Link href="/projects"><span className='text-orangeVif underline hover:cursor-pointer'>portfolio projects</span></Link>, or check out some of my <Link href="/artwork"><span className='text-orangeVif underline hover:cursor-pointer'>strange artwork</span></Link>.
                     </p>
 
                   </div>
@@ -84,28 +84,3 @@ export default function Home() {
     </>
   )
 }
-
-function WelcomeMessage({message}) {
-
-  const paragraphs = message.map((paragraph, i) => {
-    return (
-      <div className=' grid '>
-        
-      </div>
-    )
-  })
-
-  return <>{paragraphs}</>
-}
-
-const welcome = [
-  `
-    👋 Hello! Welcome to my portfolio page. My name is Scott McDonald, and I have been working incredibly hard towards becoming a web developer, and I have created this page to showcase the skills that I have been developing over the course of the past two years.
-  `,
-  `
-    This site was made in React using a Next.js framework, using Tailwind.CSS for styling, and is currently hosted on Vercel. I would like to note that this site is currently (and may forever be) in development, as I intend to use it as a sort of creative playground for new found skills and design concepts -- so you may see some changes should you revisit. 
-  `,
-  `
-    Please check out the about page for a brief summary of my journey in coding thus far, and be sure to pull up the project page for examples of my work. I am an artist by hobby, so I've included an artwork page with a few examples of my work.
-  `,
-]
